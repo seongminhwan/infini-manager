8806e240269d8cda310ec90ab4f9d21dc9096b28
seongminhwan <seongminhwan@users.noreply.github.com>
2025-05-12 00:56:45 +0800

    feat: 添加AFF返现相关路由

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -12,6 +12,8 @@ import EmailManage from '../pages/EmailManage';
 import KycImageManage from '../pages/KycImageManage';
 import RandomUserManage from '../pages/RandomUserManage';
 import AccountGroupManage from '../pages/AccountGroupManage';
+import AffCashback from '../pages/AffCashback';
+import AffHistory from '../pages/AffHistory';
 
 /**
  * 应用路由配置
@@ -26,8 +28,8 @@ const AppRoutes: React.FC = () => {
         {/* 各功能页面路由 */}
         <Route path="/overview" element={<Overview />} />
         <Route path="/account-monitor" element={<AccountMonitor />} />
-      <Route path="/account-transfer" element={<AccountTransfer />} />
-      <Route path="/account-details" element={<AccountDetails />} />
+        <Route path="/account-transfer" element={<AccountTransfer />} />
+        <Route path="/account-details" element={<AccountDetails />} />
         <Route path="/account-register" element={<AccountRegister />} />
         <Route path="/account-group-manage" element={<AccountGroupManage />} />
         <Route path="/notification-manage" element={<NotificationManage />} />
@@ -35,6 +37,9 @@ const AppRoutes: React.FC = () => {
         <Route path="/email-manage" element={<EmailManage />} />
         <Route path="/kyc-image-manage" element={<KycImageManage />} />
         <Route path="/random-user-manage" element={<RandomUserManage />} />
+        {/* AFF返现相关路由 */}
+        <Route path="/aff-cashback" element={<AffCashback />} />
+        <Route path="/aff-history" element={<AffHistory />} />
       </Route>
     </Routes>
   );
